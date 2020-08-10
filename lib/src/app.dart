27f894172e06e6abb906becdd3bbc16f91e4e949
patrickchow0803 <patrickchow0803@gmail.com>
2020-08-10 17:24:55 -0400

    Added code to retrive an image with a HTTP get request

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image+model.dart';
 
 class App extends StatefulWidget {
   @override
@@ -7,6 +11,18 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,11 +32,9 @@ class _AppState extends State<App> {
         ),
         body: Center(child: Text('# of images: $counter')),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter++;
-            });
-          },
+          // This gives onPressed a handel to the method fetchImage.
+          // DO NOT type in fetchImage(), otherwise it will cause the method to be run on startup.
+          onPressed: fetchImage,
           child: Icon(Icons.add),
         ),
       ),
